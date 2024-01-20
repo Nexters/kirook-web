f1059b3b120630c1d878943138f339c706c1cbfc
asdf99245 <asdf99245@naver.com>
2024-01-20 16:25:02 +0900

    remove: 기본 파일 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Icon } from '@/shared/components';
-
 export default function Home() {
-  return (
-    <main>
-      main
-      <Icon iconType='Pin' />
-    </main>
-  );
+  return <main>main</main>;
 }
