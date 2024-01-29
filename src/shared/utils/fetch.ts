908c5f97b1ab2264dec4723e6a9cbffe7f0e73b9
asdf99245 <asdf99245@naver.com>
2024-01-29 22:52:54 +0900

    feat: custom fetch util 추가

---
+++ src/shared/utils/fetch.ts
@@ -0,0 +1,69 @@
+type FetchUrl = string | URL | globalThis.Request;
+
+async function request<Response>(url: FetchUrl, init: RequestInit = {}): Promise<Response> {
+  const baseUrl = '';
+  const requestUrl = `${baseUrl}${url}`;
+
+  const response = await fetch(requestUrl, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    ...init,
+  });
+  if (response.status !== 200) {
+    throw new Error(`status code: ${response.status} error가 발생했습니다`);
+  }
+
+  const json = await response.json();
+  return json;
+}
+
+const get = <Response>(url: FetchUrl, options?: RequestInit) => {
+  return request<Response>(url, {
+    ...options,
+    method: 'GET',
+  });
+};
+
+const post = <Response, Body>(url: FetchUrl, body: Body, options?: RequestInit) => {
+  return request<Response>(url, {
+    ...options,
+    method: 'POST',
+    body: JSON.stringify(body),
+  });
+};
+
+const put = <Response, Body>(url: FetchUrl, body: Body, options?: RequestInit) => {
+  return request<Response>(url, {
+    ...options,
+    method: 'PUT',
+    body: JSON.stringify(body),
+  });
+};
+
+const patch = <Response, Body>(url: FetchUrl, body: Body, options?: RequestInit) => {
+  return request<Response>(url, {
+    ...options,
+    method: 'PATCH',
+    body: JSON.stringify(body),
+  });
+};
+
+const _delete = <Response, Body>(url: FetchUrl, body: Body, options?: RequestInit) => {
+  return request<Response>(url, {
+    ...options,
+    method: 'DELETE',
+    body: JSON.stringify(body),
+  });
+};
+
+const http = {
+  get,
+  post,
+  put,
+  patch,
+  delete: _delete,
+};
+
+export default http;
