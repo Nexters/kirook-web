0cac3a92914990057a9468e71e0f0ed8f4bb692d
asdf99245 <asdf99245@naver.com>
2024-02-02 23:43:30 +0900

    feat: query devtool 설정

--- src/shared/providers/QueryProvider.tsx
+++ src/shared/providers/QueryProvider.tsx
@@ -2,9 +2,15 @@
 
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
