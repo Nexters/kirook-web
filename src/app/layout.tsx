f125d4c6cdabf73d90868f9939c8697510d55632
asdf99245 <asdf99245@naver.com>
2024-02-02 23:42:15 +0900

    feat: react-query setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { QueryProvider } from '@/shared/providers/QueryProvider';
 import { pretendard } from '@/styles/fonts';
 
 export const metadata: Metadata = {
@@ -10,7 +11,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko' className={`${pretendard.variable}`}>
-      <body className={`${pretendard.className}`}>{children}</body>
+      <head />
+      <body className={`${pretendard.className}`}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
