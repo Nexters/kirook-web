23ddbfaecb2cb617764e84f690ef334aaa1c89fe
asdf99245 <asdf99245@naver.com>
2024-02-03 02:50:34 +0900

    feat: default query option setting

--- src/shared/providers/QueryProvider.tsx
+++ src/shared/providers/QueryProvider.tsx
@@ -5,7 +5,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
