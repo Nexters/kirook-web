df1cb7ff7b3635e8587d4462d06110a90e26e3b0
chaewonkong <chaewonkong@gmail.com>
2024-02-03 14:43:23 +0900

    fix: 🐛 server code for logging

--- src/app/api/db/[pageId]/route.ts
+++ src/app/api/db/[pageId]/route.ts
@@ -1,7 +1,7 @@
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { Block, Database, NotionBlockResponse } from '../../auth/interfaces';
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 export interface DatabaseResponse {
   databases: Database[];
@@ -11,22 +11,29 @@ export async function GET(request: Request, { params }: { params: { pageId: stri
   const accessToken = headers().get('Authorization') || '';
 
   // // Get databases
-  const blockUrl = `https://api.notion.com/v1/blocks/${params.pageId}/children?page_size=10`;
-  const blockResp = await axios.get<any, AxiosResponse<NotionBlockResponse>>(blockUrl, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'Notion-Version': '2022-06-28',
-    },
-  });
-  const { results } = blockResp.data;
-  const databases = results
-    .filter((v: Block) => v.type === 'child_database')
-    .map((v: Block) => {
-      if (!v.child_database?.title) {
-        // throw new Error('Database fetch error');
-      }
-
-      return { id: v.id, title: v.child_database?.title || '' };
+  try {
+    const blockUrl = `https://api.notion.com/v1/blocks/${params.pageId}/children?page_size=10`;
+    const blockResp = await axios.get<any, AxiosResponse<NotionBlockResponse>>(blockUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Notion-Version': '2022-06-28',
+      },
     });
-  return NextResponse.json<DatabaseResponse>({ databases });
+    const { results } = blockResp.data;
+    const databases = results
+      .filter((v: Block) => v.type === 'child_database')
+      .map((v: Block) => {
+        if (!v.child_database?.title) {
+          throw new Error('Database fetch error');
+        }
+
+        return { id: v.id, title: v.child_database?.title || '' };
+      });
+    return NextResponse.json<DatabaseResponse>({ databases });
+  } catch (e) {
+    const error = e as AxiosError;
+    console.log(error.message);
+
+    return new Response(error.message, { status: error.status });
+  }
 }
