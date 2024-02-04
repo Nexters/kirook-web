20087bbb52a50523d87fc91a8114df6774a8ac5e
asdf99245 <asdf99245@naver.com>
2024-02-04 18:34:12 +0900

    feat: stale time 임시 설정

--- src/shared/providers/QueryProvider.tsx
+++ src/shared/providers/QueryProvider.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+// 일단 2분으로 설정
+const QUERY_DEFAULT_STALE_TIME = 1000 * 60 * 2;
+
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
     () =>
@@ -12,6 +15,7 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
           queries: {
             refetchOnWindowFocus: false,
             retry: false,
+            staleTime: QUERY_DEFAULT_STALE_TIME,
           },
         },
       }),
