9722e23144523c973108816fc2710a6a5d6e956f
asdf99245 <asdf99245@naver.com>
2024-02-24 15:04:14 +0900

    feat: changwonDangamAsac 폰트 추가

--- src/app/(route)/link/page.tsx
+++ src/app/(route)/link/page.tsx
@@ -1,3 +1,10 @@
+import { cn } from '@/shared/utils/cn';
+import { changwonDangamAsac } from '@/styles/fonts';
+
 export default function LinkPage() {
-  return <span>link</span>;
+  return (
+    <div className='flex flex-col'>
+      <h1 className={cn('text-logo mb-5 mt-[12px]', changwonDangamAsac.className)}>Link</h1>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { QueryProvider } from '@/shared/providers/QueryProvider';
-import { pretendard } from '@/styles/fonts';
+import { changwonDangamAsac, pretendard } from '@/styles/fonts';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='ko' className={`${pretendard.variable}`}>
+    <html lang='ko' className={`${pretendard.variable} ${changwonDangamAsac.variable}`}>
       <head>
         <title>Kirook - 끼룩으로 스마트한 일상관리</title>
         <meta name='title' content='Kirook' />
