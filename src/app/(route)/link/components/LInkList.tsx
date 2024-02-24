70ef7424293d33a84c4c8fcb038035c480a8d00a
asdf99245 <asdf99245@naver.com>
2024-02-24 20:06:32 +0900

    feat: TagFilter 마크업

---
+++ src/app/(route)/link/components/LInkList.tsx
@@ -0,0 +1,7 @@
+export function LinkList() {
+  return (
+    <div className='flex grow flex-col items-center overflow-y-scroll bg-grayscale-50'>
+      <p className='mt-[158px] text-title3 text-grayscale-700'>아직 스크랩한 링크가 없어요</p>
+    </div>
+  );
+}
--- src/app/(route)/link/page.tsx
+++ src/app/(route)/link/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { LinkInput } from './components/LinkInput';
+import { TagFilter } from '@/shared/components/TagFilter';
 import { Header } from '@/shared/components/layout/Header';
-import { cn } from '@/shared/utils/cn';
 
 export default function LinkPage() {
   return (
     <div className='flex h-full flex-col'>
       <Header logoText='Link' showBackButton />
-      <div className='px-[15px] pb-[24px] pt-[12px]'>
+      <div className='px-[15px] pb-[12px] pt-[12px]'>
         <LinkInput
           onSubmit={(link) => {
             console.log(link);
@@ -16,6 +16,29 @@ export default function LinkPage() {
         />
         <div className='mt-[32px] flex items-center justify-between'>
           <strong className='text-grayscale text-black'>내 링크 목록</strong>
+        </div>
+        <div className='no-scrollbar flex gap-2 overflow-hidden overflow-x-scroll py-2'>
+          <TagFilter color='red' isSelected onClick={() => {}}>
+            tag filter
+          </TagFilter>
+          <TagFilter color='yellow' onClick={() => {}}>
+            tag filter
+          </TagFilter>
+          <TagFilter color='green' onClick={() => {}}>
+            tag filter
+          </TagFilter>
+          <TagFilter color='blue' onClick={() => {}}>
+            tag filter
+          </TagFilter>
+          <TagFilter color='purple' onClick={() => {}}>
+            tag filter
+          </TagFilter>
+        </div>
+        <div className='mt-[20px] flex justify-between'>
+          <span className='text-body2 text-grayscale-600'>n개의 링크</span>
+          <button className='text-body1 text-grayscale-900' type='button'>
+            편집하기
+          </button>
         </div>
       </div>
       <div className='flex grow flex-col items-center overflow-y-scroll bg-grayscale-50'>
